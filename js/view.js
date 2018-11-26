ff4a65505d9fcf0668514d0467058ea66d2303d1
urosh <urosh.stojkovikj@gmail.com>
2018-11-26 13:38:11 -0800

    complete exercise 4

--- js/view.js
+++ js/view.js
@@ -46,8 +46,8 @@ View.prototype.exercise3 = function () {
 View.prototype.exercise4 = function () {
   //Challenge: Write your first name in every other square.
   //Result: Your name appears in every other square.
-
   //your code here!
+  $(".square:nth-child(even)").text("uroshimaaa");
 };
 
 View.prototype.exercise5 = function () {
